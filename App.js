e53103b239d3326462b2d85e3a417ad23907c609
PhilipBurrows1 <philipburrows1234@gmail.com>
2021-03-04 19:13:41 +0000

    Add navigation bar icons
    Add navigation bar icons for all three options

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Text, View, StyleSheet} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import Home from './components/HomeScreen'
 import Search from './components/SearchScreen'
@@ -12,7 +13,23 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon:({focused,color,size})=>{
+            let iconName;
+
+            if(route.name === 'Home'){
+              iconName = focused ? 'home':'home-outline';
+            }
+            else if(route.name === 'Search'){
+              iconName = focused ? 'search':'search-outline';
+            }
+            else if(route.name === 'User'){
+              iconName = focused ? 'person':'person-outline';
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}>
         <Tab.Screen name="Home" component={Home} />
         <Tab.Screen name="Search" component={Search} />
         <Tab.Screen name="User" component={User} />
