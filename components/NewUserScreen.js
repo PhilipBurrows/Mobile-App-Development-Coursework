b6229363817bbb85270230d34533458669743572
PhilipBurrows1 <philipburrows1234@gmail.com>
2021-03-06 01:15:56 +0000

    Add create user end point
    Add create user end point functionality

--- components/NewUserScreen.js
+++ components/NewUserScreen.js
@@ -1,10 +1,44 @@
 import React , { Component } from 'react';
-import { Text, View, Button, TextInput, KeyboardAvoidingView, TouchableOpacity } from 'react-native';
+import { Text, View, Button, TextInput, KeyboardAvoidingView, TouchableOpacity, Alert } from 'react-native';
 import Icons from 'react-native-vector-icons/MaterialIcons';
 
 import BaseStyle from './BaseStyle.style';
 
 class NewUser extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      firstName:'',
+      lastName:'',
+      email:'',
+      password:''
+    }
+  }
+
+  addItem(){
+    let to_send = {
+      first_name:this.state.firstName,
+      last_name:this.state.lastName,
+      email:this.state.email,
+      password:this.state.password
+    };
+
+    return fetch('http://10.0.2.2:3333/api/1.0.0/user',{
+      method:'post',
+      headers:{'Content-Type':'application/json'},
+      body: JSON.stringify(to_send)
+    })
+    .then((response) => {
+      Alert.alert('User Created');
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
+
+
   render(){
     const navigation = this.props.navigation;
     return (
@@ -23,12 +57,21 @@ class NewUser extends Component {
           </View>
           <View style={BaseStyle.body}>
             <View style={BaseStyle.newUserInputContainer}>
-              <TextInput placeholder='Firstname' style={BaseStyle.loginInput}/>
-              <TextInput placeholder='Lastname' style={BaseStyle.loginInput}/>
-              <TextInput placeholder='Email' style={BaseStyle.loginInput}/>
-              <TextInput placeholder='Password' style={BaseStyle.loginInput}/>
+              <TextInput placeholder='Firstname' style={BaseStyle.loginInput}
+                onChangeText={(firstName) => this.setState({firstName})}
+                value={this.state.firstName}/>
+              <TextInput placeholder='Lastname' style={BaseStyle.loginInput}
+                onChangeText={(lastName) => this.setState({lastName})}
+                value={this.state.lastName}/>
+              <TextInput placeholder='Email' style={BaseStyle.loginInput}
+                onChangeText={(email) => this.setState({email})}
+                value={this.state.email}/>
+              <TextInput placeholder='Password' style={BaseStyle.loginInput}
+                onChangeText={(password) => this.setState({password})}
+                value={this.state.password}/>
               <Button
                 title='Sign Up'
+                onPress={() => this.addItem()}
               />
             </View>
           </View>
